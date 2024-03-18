8f356d9a6107eae3df9b0d0c100e99d9b0f186e2
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-03-18 17:24:31 +0000

    data for tasks added

--- js/task-4.js
+++ js/task-4.js
@@ -1,2 +1,12 @@
 'use strict';
 
+function getShippingCost(country) { 
+
+}
+
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Germany")); 
+console.log(getShippingCost("China")); 
+console.log(getShippingCost("Chile")); 
+console.log(getShippingCost("Jamaica")); 
+console.log(getShippingCost("Sweden")); 
